15ed1893e04491000feaafd706485d80e3aff193
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-12 22:28:19 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.scss';
+import Aos from "aos"
+import "aos/dist/aos.css"
+import React , { useEffect } from 'react'
 import Section from './components/Section/Section';
 import Headerinput from './Home/Headerinput/Headerinput';
 
 function App() {
+  useEffect(() =>{
+    Aos.init({});
+  }, []);
   return (
     <div className="App">
        <Headerinput />
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,25 +4,22 @@ const Section =()=>{
 return(
 <section className="p-4">
     <div className="container">
-        <div className="row row-cols-2 row-cols-md-4 g-4 text-center" style="align-items: center;">
-            <div className="col">
-                <p className='section__number' style="font-size: 90px; line-height: 100px; font-weight: bold; color: #01384D;">7</p>
-                <p style="font-size: 24px;line-height: 26px; color: #01384D;font-weight: 500;">yillik tajriba</p>
+        <div className="row row-cols-2 row-cols-md-4 g-4 text-center">
+            <div className="section__page col">
+                <p className='section__number'>7</p>
+                <p className='section__text'>yillik tajriba</p>
             </div>
-            <div className="col">
-                <p className='section__numbers' style="font-size: 80px; line-height: 80px; font-weight:bold;color: #01384D;">10k+</p>
-                <p style="font-size: 24px;line-height: 24px; color: #01384D;font-weight: 500;">mamnun mijozlar</p>
-
+            <div className="section__page col">
+                <p className='section__numbers'>10k+</p>
+                <p className='section__text'>mamnun mijozlar</p>
             </div>
-            <div className="col">
-                <p className='section__number' style="font-size: 90px; line-height: 100px; font-weight: bold; color: #01384D;">10</p>
-                <p style="font-size: 24px;line-height: 26px; color: #01384D;font-weight: 500;">yillik kafolat</p>
-
+            <div className="section__page col">
+                <p className='section__number'>10</p>
+                <p className='section__text'>yillik kafolat</p>
             </div>
-            <div className="col">
-                <p className='section__number' style="font-size: 90px; line-height: 90px; font-weight: bold; color: #01384D;">3</p>
-                <p style="font-size: 20px;line-height: 22px; color: #01384D;font-weight: 500;">kunda yetkazish</p>
-
+            <div className="section__page col">
+                <p className='section__number'>3</p>
+                <p className='section__text'>kunda yetkazish</p>
             </div>
         </div>
     </div>
