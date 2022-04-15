80854c7fe97166cbe6a66775246ad8f699940f74
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-15 21:16:00 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,16 @@ import './App.scss';
 import Aos from "aos"
 import "aos/dist/aos.css"
 import React, {useEffect}  from 'react'
-
-// import { useRef, useState } from "react";
 import Section from './components/Section/Section';
 import Headerinput from './Home/Headerinput/Headerinput';
 import SectionPage from './components/SectionPage/SectionPage';
 import Sublime from './components/Sublime/Sublime';
 import Aside from './components/Aside/Aside';
 import Box from './components/Box/Box'; 
-// import matras from './assets/img/matras.png'
+// import Navbar from './components/Navbar/Navbar';
 
-function App() {
 
-  var myVideo = document.getElementById("video1"); 
-
-const playPause =()  =>{ 
-  if (myVideo.paused) 
-    myVideo.play(); 
-  else 
-    myVideo.pause(); 
-} 
-
-const makeBig =() => { 
-    myVideo.width = 560; 
-}  
+function App() { 
   useEffect(() =>{
     Aos.init({});
   }, []);
@@ -36,7 +22,8 @@ const makeBig =() => {
        <SectionPage />
        <Sublime />
        <Aside />
-       <Box />
+       {/* <Box /> */}
+       {/* <Navbar /> */}
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import './Navbar.scss'
+
+import matras3 from '../../assets/img/matras3.png'
+import button from '../../assets/img/Polygon.png'
+
+const Navbar =()=>{
+    return(
+        <div className="navbar">
+            <div className="container">
+            <div className="row align-items-center g-0">
+                <div className="col-xl-5 col-lg-6 col-md-12">
+                    <div className="py-5 py-lg-0">
+                       <p className='navbar__name'> Mattrassue kompaniyasi haqida</p>
+                       <p className='navbar__text' >Penatibus viverra gravida rhoncus in. At turpis morbi ante tortor a est. Habitant adipiscing ut sed pulvinar tellus, ut urna, fermentum:</p>
+                    </div>
+                    <div className="navbar__title py-5 py-lg-0" >
+                        <p className=" navbar__text  m-3">Penatibus viverra gravida rhoncus in. At turpis morbi ante tortor.</p>
+                        <p className=" navbar__text m-3">Dolor integer in interdum viverra risus dolor enim.</p>
+                        <p className=" navbar__text m-3">Turpis senectus eu, eget aenean nulla pellentesque sed ut tempor.</p>
+                    </div>
+                </div>
+                <div data-aos="zoom-out-left" className=" col-xl-7 col-lg-6 col-md-10 text-lg-end text-center">
+                    <button className="navbar__btns">
+                          <img className='navbar__img' src={button} alt="" />
+                      </button>
+                    <img  src={matras3} alt="" className="img-fluid"  />
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
