867c9d967af83b5cac65f89b1930f9ba804240cc
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-15 14:39:41 +0500

    commit

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,10 +1,57 @@
 import './Box.scss'
 
+//Images
+import matras from '../../assets/img/matras.png'
+import matras1 from '../../assets/img/matras2.png'
+import matras2 from '../../assets/img/matras1.png'
+import button from '../../assets/img/Polygon.png'
+
 const Box = ()=>{
     return(
         <div className="box">
             <div className="container">
-                
+                <p className="box__names">
+                    Ishlab chiqarish tehnologiyalari
+                </p>
+
+                <div className="p-5 row row-cols-1 row-cols-md-3 g-4">
+                <div className="col">
+                      <p className='box__name'>Memoriform</p>
+                  <div className="card ">
+                        <button className="box__btn">
+                          <img className='box__img' src={button} alt="" />
+                        </button>
+                    <img src={matras} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <p className="card-text box__text text-center">Lectus pellentesque senectus elit donec massa ipsum ultricies dui. Bibendum et enim fringilla tincidunt ligula non, condimentum nunc.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                     <p className='box__name'>Tabiiy lateks</p>
+                  <div className="card  ">
+                        <button className="box__btn">
+                          <img className='box__img' src={button} alt="" />
+                        </button>
+                    <img src={matras1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <p className="card-text box__text text-center">Aliquam euismod ornare justo, sed faucibus eu. Sed amet tellus netus quis bibendum. Euismod diam eu sem tristique aenean rhoncus.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                    <p className='box__name'>Mustaqil prujina</p>
+                  <div className="card ">
+                      <button className="box__btn">
+                          <img className='box__img' src={button} alt="" />
+                      </button>
+                       <img src={matras2} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <p className="card-text box__text text-center">Enim urna consequat, justo, cras tincidunt imperdiet orci sodales. Dui purus feugiat morbi quam orci, vel. Elementum  ultricies venenatis rhoncus.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
     )
