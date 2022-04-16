ed7704ff54d0a13bde39b5c1105d5ef656ce5981
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-16 21:34:52 +0500

    new file

---
+++ src/components/Sublimepage/Sublimepage.jsx
@@ -0,0 +1,13 @@
+import './Sublimepage.scss'
+
+const Sublimepage =() =>{
+    return(
+        <div className="sublimepage">
+            <div className="container">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Sublimepage
