b26665367a20670f7ca11ae17412fd8cc3358eed
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-26 23:09:26 +0500

    Initial commit

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,27 +1,20 @@
 import './Box.scss'
 
 //Images
-import matras from '../../assets/img/matras.png'
-import matras1 from '../../assets/img/matras2.png'
-import matras2 from '../../assets/img/matras1.png'
-import button from '../../assets/img/Polygon.png'
 import video from '../../assets/img/video3.mp4'
 
 const Box = ()=>{
     return(
         <div className="box">
             <div className="container">
-                <p className="box__names">
+                <p className="box__names mt-5 pt-5">
                     Ishlab chiqarish tehnologiyalari
                 </p>
 
-                <div className="p-5 row row-cols-1 row-cols-md-3 g-4">
+                <div className="p-5  row row-cols-1 row-cols-md-4 g-4 ">
                 <div className="col">
-                      <p className='box__name'>Memoriform</p>
-                  <div className="card ">
-                        {/* <button className="box__btn">
-                          <img className='box__img' src={button} alt="" />
-                        </button> */}
+                      <p className='box__name '>Memoriform</p>
+                  <div className="card box__list">
                     <video className="box__video feedback-video" controls preload="metadata" src={video}
                      type="video/mp4"></video>
                     <div className="card-body">
@@ -29,12 +22,9 @@ const Box = ()=>{
                     </div>
                   </div>
                 </div>
-                <div className="col">
+                <div className="col ">
                      <p className='box__name'>Tabiiy lateks</p>
-                  <div className="card  ">
-                        {/* <button className="box__btn">
-                          <img className='box__img' src={button} alt="" />
-                        </button> */}
+                  <div className="card box__list ">
                     <video className="box__video feedback-video" controls preload="metadata" src={video}
                      type="video/mp4"></video>
                     <div className="card-body">
@@ -44,10 +34,7 @@ const Box = ()=>{
                 </div>
                 <div className="col">
                     <p className='box__name'>Mustaqil prujina</p>
-                  <div className="card ">
-                      {/* <button className="box__btn">
-                          <img className='box__img' src={button} alt="" />
-                      </button> */}
+                  <div className="card box__list">
                        <video className="box__video feedback-video" controls preload="metadata" src={video}
                      type="video/mp4"></video>
                     <div className="card-body">
