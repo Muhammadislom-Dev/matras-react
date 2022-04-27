c2182c72ed846ce33221a090264c178dcab1a68c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-27 16:02:31 +0500

    Initial commit

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -10,28 +10,28 @@ const Swiper = () =>{
             <div className="container">
                  <p className="swiper__name">Nega bizni tanlashdi</p>
 
-                 <div className=" row row-cols-1 row-cols-md-3 g-3">
-                <div className="col ">
+                 <div className=" row row-cols-1 row-cols-md-3 g-1 mt-5">
+                <div className="col swiper__lists">
                   <div className=" card swiper__list ">
-                    <img src={car} className="card-img-top" alt="..." />
+                    <img src={car} className="card-img-top  swiper__img" alt="..." />
                     <div className="card-body">
                       <h5 className="swiper__names card-title text-center" >Yetkazib berish</h5>
                       <p className="swiper__text  card-text text-center" >Toshkent bo'ylab bepul o'lchov va yetkazib berish</p>
                     </div>
                   </div>
                 </div>
-                <div className="col ">
+                <div className="col swiper__lists">
                   <div className=" card swiper__list ">
-                    <img src={person} className="card-img-top" alt="..." />
+                    <img src={person} className="card-img-top  swiper__img" alt="..." />
                     <div className="card-body">
                       <h5 className="swiper__names card-title text-center" >Qo'llab quvvatlash</h5>
                       <p className="swiper__text card-text text-center" >Bizning qollab-quvvatlash xizmati sizga har qanday savolda yordam beradi va menejerlarning</p>
                     </div>
                   </div>
                 </div>
-                <div className="col ">
+                <div className="col swiper__lists">
                   <div className=" card swiper__list ">
-                    <img src={person1} className="card-img-top" alt="..." />
+                    <img src={person1} className="card-img-top swiper__img" alt="..." />
                     <div className="card-body">
                       <h5 className="swiper__names card-title text-center"  >Kafolat</h5>
                       <p className="swiper__text card-text text-center" >Biz matraslarimiz uchun 8 yilgacha kafolat beramiz. Agar matras kamida 25mm qisqartirilsa.</p>
